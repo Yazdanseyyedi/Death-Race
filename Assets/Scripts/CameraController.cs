c53a97e56b59166ef8065b321c4cdfae1b3385e7
arisaghafari <arisa_ghafari@outlook.com>
2021-12-31 17:38:41 +0330

    implimented Split Screen Camera for each player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController car;
+    void Update()
+    {
+        transform.position = new Vector3(car.transform.position.x, car.transform.position.y, transform.position.z);
+    }
+}
