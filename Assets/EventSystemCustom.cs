7d81f18d8579d8c1de4c4b3ddcb383eca1ac7ff2
arisaghafari <arisa_ghafari@outlook.com>
2022-01-19 11:53:57 +0330

    modified eventsystem

--- Assets/EventSystemCustom.cs
+++ Assets/EventSystemCustom.cs
@@ -8,11 +8,15 @@ public class EventSystemCustom : MonoBehaviour
     public UnityEvent onShotDamage;
     public UnityEvent onRocketDamage;
     public UnityEvent onMineDamage;
+    public UnityEvent PlayerOneCycleEnter;
+    public UnityEvent PlayerTwoCycleEnter;
 
     void Awake()
     {
         onShotDamage = new UnityEvent();
         onRocketDamage = new UnityEvent();
         onMineDamage = new UnityEvent();
+        PlayerOneCycleEnter = new UnityEvent();
+        PlayerTwoCycleEnter = new UnityEvent();
     }
 }
